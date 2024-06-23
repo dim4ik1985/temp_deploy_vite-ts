4b8bf029c80238a806c47e4360aecc4391bd5700
dim4ik1985 <dim4ik020985@gmail.com>
2024-06-23 13:01:31 +0300

    edit config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/deploy_gh-action_temp_vite-TS/"
+  base: "/temp_deploy_vite-ts/"
 });
